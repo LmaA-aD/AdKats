1e5adcf7e5dc824992f718c3c6589151a12b41f4
ColColonCleaner <danielgradinjan@gmail.com>
2013-06-03 16:13:24 -0500

    0.2.5.0 Partial Update

--- update.sql
+++ update.sql
@@ -1,5 +1,6 @@
 ALTER TABLE `adkat_records` MODIFY `record_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
-ALTER TABLE `adkat_records` CHANGE `server_id` `server_ip` varchar(45) NOT NULL DEFAULT "0.0.0.0:0000";
+ALTER TABLE `adkat_records` MODIFY `server_id` int(11) NOT NULL DEFAULT -1, 
+ALTER TABLE `adkat_records` ADD `server_ip` varchar(45) NOT NULL DEFAULT "0.0.0.0:0000";
 ALTER TABLE `adkat_records` MODIFY `command_type` varchar(45) NOT NULL DEFAULT "DefaultCommand"; 
 ALTER TABLE `adkat_records` ADD `command_action` varchar(45) NOT NULL DEFAULT "DefaultAction"; 
 ALTER TABLE `adkat_records` MODIFY `record_durationMinutes` int(11) NOT NULL DEFAULT 0; 
@@ -8,6 +9,7 @@ ALTER TABLE `adkat_records` MODIFY `target_name` varchar(45) NOT NULL DEFAULT "N
 ALTER TABLE `adkat_records` MODIFY `source_name` varchar(45) NOT NULL DEFAULT "NoNameAdmin"; 
 ALTER TABLE `adkat_records` MODIFY `record_message` varchar(100) NOT NULL DEFAULT "NoMessage"; 
 ALTER TABLE `adkat_records` MODIFY `adkats_read` ENUM('Y', 'N') NOT NULL DEFAULT 'N';
+DROP TABLE `adkat_teamswapwhitelist`;
 CREATE TABLE `adkat_accesslist` ( 
 `player_name` varchar(45) NOT NULL DEFAULT "NoPlayer", 
 `player_guid` varchar(100) NOT NULL DEFAULT 'WAITING ON USE FOR GUID', 
