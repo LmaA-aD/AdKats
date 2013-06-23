2ee2158d85528c0a9218f532fc5a552a8a8d38f7
ColColonCleaner <danielgradinjan@gmail.com>
2013-06-22 21:55:27 -0500

    Update adkats.sql

--- adkats.sql
+++ adkats.sql
@@ -3,30 +3,6 @@
 -- This is run automatically if AdKats senses the database is not set up properly.
 -- If you don't want the plugin changing tables/views in your database, you must run this beforehand.
 
--- If the tables needed are not in the database yet, the below two queries will be success
-CREATE TABLE IF NOT EXISTS `adkat_records` ( 
-       `record_id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT, 
-       `server_id` INT(11) UNSIGNED NOT NULL, 
-       `command_type` VARCHAR(45) NOT NULL DEFAULT "DefaultCommand", 
-       `command_action` VARCHAR(45) NOT NULL DEFAULT "DefaultAction", 
-       `player_id` INT(11) UNSIGNED NOT NULL, 
-       `source_name` VARCHAR(45) NOT NULL DEFAULT "NoNameAdmin", 
-       `record_message` VARCHAR(100) NOT NULL DEFAULT "NoMessage", 
-       `record_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
-       `adkats_read` ENUM('Y', 'N') NOT NULL DEFAULT 'N', 
-       PRIMARY KEY (`record_id`), 
-       CONSTRAINT `fk_server_id` 
-              FOREIGN KEY (`server_id` ) 
-              REFERENCES `tbl_server`.`ServerID` 
-              ON DELETE CASCADE 
-              ON UPDATE NO ACTION, 
-       CONSTRAINT `fk_player_id` 
-              FOREIGN KEY (`player_id` ) 
-              REFERENCES `tbl_playerdata`.`PlayerID` 
-              ON DELETE CASCADE 
-              ON UPDATE NO ACTION
-);
-
 CREATE TABLE IF NOT EXISTS `adkat_accesslist` ( 
        `player_name` VARCHAR(45) NOT NULL DEFAULT "NoPlayer", 
        `player_guid` VARCHAR(100) NOT NULL DEFAULT 'WAITING ON USE FOR GUID', 
@@ -34,37 +10,139 @@ CREATE TABLE IF NOT EXISTS `adkat_accesslist` (
        PRIMARY KEY (`player_name`), UNIQUE KEY `player_name_UNIQUE` (`player_name`)
 );
 
-CREATE TABLE IF NOT EXISTS `adkat_banlist` ( 
-       `ban_id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT, 
-       `record_id` INT(11) UNSIGNED NOT NULL, 
-       `player_id` INT(11) UNSIGNED NOT NULL, 
-       `server_id` INT(11) UNSIGNED NOT NULL, 
-       `ban_status` enum('Enabled', 'Disabled') NOT NULL DEFAULT 'Enabled',
-       `ban_reason` VARCHAR(100) NOT NULL DEFAULT 'NoReason', 
-       `ban_notes` VARCHAR(150) NOT NULL DEFAULT 'NoNotes', 
-       `ban_sync` VARCHAR(100) NOT NULL DEFAULT "-sync-", 
-       `ban_startTime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, 
-       `ban_endTime` DATETIME NOT NULL, 
-       `ban_displayDurationMinutes` int(11) NOT NULL DEFAULT 0, 
-       PRIMARY KEY (`ban_id`), 
-       UNIQUE KEY `ban_id_UNIQUE` (`ban_id`), 
-       UNIQUE KEY `player_id_UNIQUE` (`player_id`), 
-       CONSTRAINT `fk_player_id` 
-              FOREIGN KEY (`player_id` ) 
-              REFERENCES `tbl_playerdata`.`PlayerID` 
-              ON DELETE CASCADE 
-              ON UPDATE NO ACTION, 
-       CONSTRAINT `fk_server_id` 
-              FOREIGN KEY (`server_id` ) 
-              REFERENCES `tbl_server`.`ServerID` 
-              ON DELETE CASCADE 
-              ON UPDATE NO ACTION, 
-       CONSTRAINT `fk_record_id` 
-              FOREIGN KEY (`record_id` ) 
-              REFERENCES `adkat_records`.`record_id` 
-              ON DELETE CASCADE 
-              ON UPDATE NO ACTION
+CREATE TABLE IF NOT EXISTS `adkat_records` ( 
+       `record_id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT, 
+	`server_id` INT(11) UNSIGNED NOT NULL, 
+	`command_type` VARCHAR(45) NOT NULL DEFAULT "DefaultCommand", 
+	`command_action` VARCHAR(45) NOT NULL DEFAULT "DefaultAction", 
+	`player_id` INT(11) UNSIGNED NOT NULL, 
+	`source_name` VARCHAR(45) NOT NULL DEFAULT "NoNameAdmin", 
+	`record_message` VARCHAR(100) NOT NULL DEFAULT "NoMessage", 
+	`record_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+	`adkats_read` ENUM('Y', 'N') NOT NULL DEFAULT 'N', 
+	PRIMARY KEY (`record_id`), 
+	CONSTRAINT `fk_server_id` 
+		FOREIGN KEY (`server_id` ) 
+		REFERENCES `tbl_server`.`ServerID` 
+		ON DELETE CASCADE 
+		ON UPDATE NO ACTION, 
+	CONSTRAINT `fk_player_id` 
+		FOREIGN KEY (`player_id` ) 
+		REFERENCES `tbl_playerdata`.`PlayerID` 
+		ON DELETE CASCADE 
+		ON UPDATE NO ACTION
 );
+
+CREATE TABLE IF NOT EXISTS `adkat_serverPlayerPoints` (
+	`player_id` INT(11) UNSIGNED NOT NULL, 
+	`server_id` INT(11) UNSIGNED NOT NULL, 
+	`punish_points` INT(11) UNSIGNED NOT NULL, 
+	`forgive_points` INT(11) UNSIGNED NOT NULL, 
+	`total_points` INT(11) UNSIGNED NOT NULL, 
+	PRIMARY KEY (`player_id`, `server_id`), 
+	CONSTRAINT `fk_server_id` 
+		FOREIGN KEY (`server_id` ) 
+		REFERENCES `tbl_server`.`ServerID` 
+		ON DELETE CASCADE 
+		ON UPDATE NO ACTION, 
+	CONSTRAINT `fk_player_id` 
+		FOREIGN KEY (`player_id` ) 
+		REFERENCES `tbl_playerdata`.`PlayerID` 
+		ON DELETE CASCADE 
+		ON UPDATE NO ACTION
+);
+
+CREATE TABLE IF NOT EXISTS `adkat_globalPlayerPoints` (
+	`player_id` INT(11) UNSIGNED NOT NULL, 
+	`punish_points` INT(11) UNSIGNED NOT NULL, 
+	`forgive_points` INT(11) UNSIGNED NOT NULL, 
+	`total_points` INT(11) UNSIGNED NOT NULL, 
+	PRIMARY KEY (`player_id`), 
+	CONSTRAINT `fk_player_id` 
+		FOREIGN KEY (`player_id` ) 
+		REFERENCES `tbl_playerdata`.`PlayerID` 
+		ON DELETE CASCADE 
+		ON UPDATE NO ACTION
+);
+
+CREATE TABLE IF NOT EXISTS `adkat_banlist` ( 
+	`ban_id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT, 
+	`record_id` INT(11) UNSIGNED NOT NULL, 
+	`player_id` INT(11) UNSIGNED NOT NULL, 
+	`server_id` INT(11) UNSIGNED NOT NULL, 
+	`ban_status` enum('Enabled', 'Disabled') NOT NULL DEFAULT 'Enabled',
+	`ban_reason` VARCHAR(100) NOT NULL DEFAULT 'NoReason', 
+	`ban_notes` VARCHAR(150) NOT NULL DEFAULT 'NoNotes', 
+	`ban_sync` VARCHAR(100) NOT NULL DEFAULT "-sync-", 
+	`ban_startTime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+	`ban_endTime` DATETIME NOT NULL, 
+	`ban_displayDurationMinutes` int(11) NOT NULL DEFAULT 0, 
+	PRIMARY KEY (`ban_id`), 
+	UNIQUE KEY `ban_id_UNIQUE` (`ban_id`), 
+	UNIQUE KEY `player_id_UNIQUE` (`player_id`), 
+	CONSTRAINT `fk_player_id` 
+		FOREIGN KEY (`player_id` ) 
+		REFERENCES `tbl_playerdata`.`PlayerID` 
+		ON DELETE CASCADE 
+		ON UPDATE NO ACTION, 
+	CONSTRAINT `fk_server_id` 
+		FOREIGN KEY (`server_id` ) 
+		REFERENCES `tbl_server`.`ServerID` 
+		ON DELETE CASCADE 
+		ON UPDATE NO ACTION, 
+	CONSTRAINT `fk_record_id` 
+		FOREIGN KEY (`record_id` ) 
+		REFERENCES `adkat_records`.`record_id` 
+		ON DELETE CASCADE 
+		ON UPDATE NO ACTION
+);
+
+DROP TRIGGER IF EXISTS update_points_trigger;
+delimiter |
+CREATE TRIGGER update_points_trigger AFTER INSERT ON `adkat_records`
+	FOR EACH ROW 
+	BEGIN 
+		DECLARE command_type VARCHAR(45);
+		DECLARE server_id INT(11);
+		DECLARE player_id INT(11);
+		SET command_type = NEW.command_type;
+		SET server_id = NEW.server_id;
+		SET player_id = NEW.player_id;
+
+		IF(command_type = 'Punish') THEN
+			INSERT INTO `adkat_serverPlayerPoints` 
+				(`player_id`, `server_id`, `punish_points`, `forgive_points`, `total_points`) 
+			VALUES 
+				(player_id, server_id, 1, 0, 1) 
+			ON DUPLICATE KEY UPDATE 
+				`punish_points` = `punish_points` + 1, 
+				`total_points` = `total_points` + 1;
+			INSERT INTO `adkat_globalPlayerPoints` 
+				(`player_id`, `punish_points`, `forgive_points`, `total_points`) 
+			VALUES 
+				(player_id, 1, 0, 1) 
+			ON DUPLICATE KEY UPDATE 
+				`punish_points` = `punish_points` + 1, 
+				`total_points` = `total_points` + 1;
+		ELSE 
+			INSERT INTO `adkat_serverPlayerPoints` 
+				(`player_id`, `server_id`, `punish_points`, `forgive_points`, `total_points`) 
+			VALUES 
+				(player_id, server_id, 0, 1, -1) 
+			ON DUPLICATE KEY UPDATE 
+				`forgive_points` = `forgive_points` + 1, 
+				`total_points` = `total_points` - 1;
+			INSERT INTO `adkat_globalPlayerPoints` 
+				(`player_id`, `punish_points`, `forgive_points`, `total_points`) 
+			VALUES 
+				(player_id, 0, 1, -1) 
+			ON DUPLICATE KEY UPDATE 
+				`forgive_points` = `forgive_points` + 1, 
+				`total_points` = `total_points` - 1;
+		END IF;
+	END;
+|
+delimiter ;
 
 CREATE OR REPLACE VIEW `adkat_playerlist` AS
 SELECT `adkat_records`.`target_name` AS `player_name`,
@@ -207,16 +285,3 @@ SELECT
 
   (SELECT COUNT(*)
    FROM adkat_records) AS 'TotalCommands';
-
--- Run these last, if they fail it means it was a new DB, if it succeeds their previous data was saved.
-ALTER TABLE `adkat_records` MODIFY `record_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
-ALTER TABLE `adkat_records` MODIFY `server_id` int(11) NOT NULL DEFAULT -1;
-ALTER TABLE `adkat_records` MODIFY `command_type` varchar(45) NOT NULL DEFAULT "DefaultCommand"; 
-ALTER TABLE `adkat_records` MODIFY `record_durationMinutes` int(11) NOT NULL DEFAULT 0; 
-ALTER TABLE `adkat_records` MODIFY `target_guid` varchar(100) NOT NULL DEFAULT "EA_NoGUID"; 
-ALTER TABLE `adkat_records` MODIFY `target_name` varchar(45) NOT NULL DEFAULT "NoTarget"; 
-ALTER TABLE `adkat_records` MODIFY `source_name` varchar(45) NOT NULL DEFAULT "NoNameAdmin"; 
-ALTER TABLE `adkat_records` MODIFY `record_message` varchar(100) NOT NULL DEFAULT "NoMessage"; 
-ALTER TABLE `adkat_records` MODIFY `adkats_read` ENUM('Y', 'N') NOT NULL DEFAULT 'N';
-
-DROP TABLE IF EXISTS `adkat_teamswapwhitelist`;
