7100b84d047d84fcc3f8f200763dd603f855d730
ColColonCleaner <danielgradinjan@gmail.com>
2013-11-25 03:37:11 -0600

    Update adkats.sql

--- adkats.sql
+++ adkats.sql
@@ -2,6 +2,51 @@ SET FOREIGN_KEY_CHECKS=0;
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
+DELIMITER //
+
+DROP PROCEDURE IF EXISTS addLogPlayerID //
+CREATE PROCEDURE addLogPlayerID()
+BEGIN
+
+-- add logSoldierID column safely
+IF NOT EXISTS( (SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA=DATABASE()
+        AND COLUMN_NAME='logPlayerID' AND TABLE_NAME='tbl_chatlog') ) THEN
+    ALTER TABLE `tbl_chatlog` ADD COLUMN `logPlayerID` INT(10) UNSIGNED DEFAULT NULL AFTER `logSubset`;
+	ALTER TABLE `tbl_chatlog` ADD INDEX (`logPlayerID`);
+	ALTER TABLE `tbl_chatlog` ADD CONSTRAINT `tbl_chatlog_ibfk_player_id` FOREIGN KEY (`logPlayerID`) REFERENCES `tbl_playerdata` (`PlayerID`) ON DELETE CASCADE ON UPDATE CASCADE;
+	UPDATE 
+		`tbl_chatlog`
+	INNER JOIN 
+		`tbl_playerdata`
+	ON 
+		`tbl_chatlog`.`logSoldierName` = `tbl_playerdata`.`SoldierName` 
+	SET 
+		`tbl_chatlog`.`logPlayerID` = `tbl_playerdata`.`PlayerID`
+	WHERE 
+		`tbl_playerdata`.`SoldierName` <> 'AutoAdmin' 
+	AND 
+		`tbl_playerdata`.`SoldierName` <> 'AdKats' 
+	AND 
+		`tbl_playerdata`.`SoldierName` <> 'Server' 
+	AND 
+		`tbl_playerdata`.`SoldierName` <> 'BanEnforcer'
+	AND 
+		`tbl_chatlog`.`logPlayerID` IS NULL;
+END IF;
+
+END //
+
+CALL addLogPlayerID() //
+
+DROP TRIGGER IF EXISTS `tbl_chatlog_player_id_insert`//
+CREATE TRIGGER `tbl_chatlog_player_id_insert` BEFORE INSERT ON `tbl_chatlog`
+ FOR EACH ROW BEGIN 
+        SET NEW.logPlayerID = (SELECT `PlayerID` FROM `tbl_playerdata` WHERE `SoldierName` = NEW.logSoldierName LIMIT 1);
+    END
+//
+
+DELIMITER ;
+
 DROP TABLE IF EXISTS `adkats_bans`;
 CREATE TABLE IF NOT EXISTS `adkats_bans` (
   `ban_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
